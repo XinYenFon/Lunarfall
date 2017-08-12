e12c22a89de0c3963011fc383cea2d174ecf820a
Mert ALINBAY <antes@simplemachines.org>
2017-08-12 03:21:29 +0300

    a small patch for font-icons.

--- Lunarfall/GenericMenu.template.php
+++ Lunarfall/GenericMenu.template.php
@@ -96,7 +96,7 @@ function template_generic_menu(&$menu_context)
 								<li', !empty($area['subsections']) ? ' class="subsections"' : '', '>';
 
 			echo '
-									<a class="', $area['icon_class'], !empty($area['selected']) ? ' chosen ' : '', '" href="', (isset($area['url']) ? $area['url'] : $menu_context['base_url'] . ';area=' . $i), $menu_context['extra_parameters'], '"><i class="fa fa-',  $area['icon_class_plain'],' fa-lg"></i> ',$area['label'], '</a>';
+									<a class="', $area['icon_class'], !empty($area['selected']) ? ' chosen ' : '', '" href="', (isset($area['url']) ? $area['url'] : $menu_context['base_url'] . ';area=' . $i), $menu_context['extra_parameters'], '"><i class="fa fa-',  $area['plain_class'],' fa-lg"></i> ',$area['label'], '</a>';
 
 			// Is this the current area, or just some area?
 			if (!empty($area['selected']) && empty($context['tabs']))
