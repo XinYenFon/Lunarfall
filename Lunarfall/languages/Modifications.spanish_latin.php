18032f043b913b45164030bd3aaa34a06624990b
Bomber Code <contacto@bombercode.net>
2019-01-12 22:01:30 -0300

    Translation to Latin Spanish!

---
+++ Lunarfall/languages/Modifications.spanish_latin.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.1 Beta 4; Modifications
+// Spanish translation by https://www.bombercode.net Copyright 2014-2018
+
+// Lunarfall Post
+$txt['lunarfall_facebook'] = 'Compartir en Facebook';
+$txt['lunarfall_twitter'] = 'Compartir en Twitter';
+$txt['lunarfall_gplus'] = 'Compartir en Google+';
+?>
