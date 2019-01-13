d168c09773ef7001a3bdf9d6a1c3e28ba24cedfb
Bomber Code <contacto@bombercode.net>
2019-01-12 22:09:09 -0300

    Translation to Latin Spanish

---
+++ Lunarfall/languages/Settings.spanish_latin.php
@@ -0,0 +1,18 @@
+<?php
+// Version: 2.1 Beta 4; Settings
+// Spanish translation by https://www.bombercode.net Copyright 2014-2018
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.png';
+$txt['theme_description'] = 'Lunafall es un juego oscuro inspirado en el sitio web de Steam.';
+$txt['facebook_page'] = 'Pagina de Facebook';
+$txt['fb_page_desc'] = '<span class="smalltext">No ingrese la URL completa, solo use la última parte. ej.; .../<strong>smforum</strong></span>';
+$txt['twitter_page'] = 'Página de Twitter';
+$txt['twitter_desc'] = '<span class="smalltext">No ingrese la URL completa, solo use la última parte. ej.; .../<strong>simplemachines</strong></span>';
+$txt['googleplus_page'] = 'Página de Google+';
+$txt['gplus_desc'] = '<span class="smalltext">No ingrese la URL completa, solo use la última parte. ej.; .../<strong>+SMFforum</strong> or .../<strong>118330618266939737270</strong></span>';
+$txt['disable_fa_icons'] = 'Desactivar los iconos Font-Awesome';
+$txt['disable_fa_icons_desc'] = '<span class="smalltext"Si no desea que los iconos Font-Awesome en su sitio se puede desactivar aquí<br>Aumenta el rendimiento al no cargar los archivos relacionados con Font-Awesome</span>';
+
+?>
